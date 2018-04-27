836ca5a7319f4c06df641b5baf55c23ac7e4fb2f
Hannah Lee <hanlavlee@gmail.com>
2018-04-27 11:18:51 -0700

    add projects views files and embedded ruby logic.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
   validates :project_name, :presence => true
-  validates :cost, :project_technology => true
+  validates :project_technology, :presence => true
   validates :project_description, :presence => true
 end
