6ae60d3a7bb3b056f6d3a4141b24f9cb3dc63d33
Hannah <leexhannah@gmail.com>
2018-04-28 16:40:50 -0700

    uncomment project.create in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ Project.destroy_all
 Skill.destroy_all
 
 
-# Project.create!(project_name: "Test Project", project_technology: "JavaScript, Angular2", project_description: "yadda yadda yadda")
+Project.create!(project_name: "Test Project", project_technology: "JavaScript, Angular2", project_description: "yadda yadda yadda")
 
 Skill.create!(skill_name: "Interpersonal Communication", skill_description: "Pair Programming")
