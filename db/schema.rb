9c5c04200cf5441859d1cc2ae4782bd9083b54d6
Hannah Lee <hanlavlee@gmail.com>
2018-05-24 00:06:50 -0700

    update image files, add deployed column, work on centering and styling index and show pages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_14_023353) do
+ActiveRecord::Schema.define(version: 2018_05_24_055618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_05_14_023353) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.string "github"
+    t.string "deployed"
   end
 
   create_table "skills", force: :cascade do |t|
